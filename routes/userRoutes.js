50eb26a721ece925b566b5aa1b045f8f4c3f0a7e
Ameer khan B <106725517+itisameerkhan@users.noreply.github.com>
2024-04-11 08:09:29 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { getUsers, setUsers } from "../controllers/usersController.js";
 
 const router = express.Router();  
-
 router.route("/").get(getUsers).post(setUsers);
 
 export default router;
